d9ea023b40ace18fd6b1d8eae2edcb4fbe7a0fab
周君 <410919244@qq.com>
2018-07-19 10:35:12 +0800

    zhoujun

--- wmeimob.core/src/main/java/com/wmeimob/exception/AuthorityException.java
+++ wmeimob.core/src/main/java/com/wmeimob/exception/AuthorityException.java
@@ -14,10 +14,36 @@ public class AuthorityException extends RuntimeException {
 	
 	public AuthorityException() {}
 
-	public AuthorityException(String msg) {
+	private AuthorityException(String msg) {
 		super(msg);
 		this.msg = msg;
 	}
+	
+	private AuthorityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+	
+	/**
+	 * 返回异常
+	 * @param message
+	 * @return
+	 * @author zJun
+	 * @date 2018年4月5日 下午10:41:05
+	 */
+	public static AuthorityException getInstance(String message) {
+        return new AuthorityException(message);
+    }
+	
+	/**
+     * 返回异常
+     * @param result
+     * @return
+     * @author zJun
+     * @date 2018年4月5日 下午8:23:13
+     */
+    public static AuthorityException getInstance(String message, Throwable cause) {
+            return new AuthorityException(message, cause);
+    }
 
 	public String getMsg() {
 		return msg;
